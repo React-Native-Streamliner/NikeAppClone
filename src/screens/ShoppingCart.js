0b42c7cdc9415ad91794a666198e441cf1c26548
tiger <thisisstreamliner@gmail.com>
2023-09-24 23:44:51 +0300

    Create Shopping Footer

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,4 +1,4 @@
-import { FlatList, Text } from "react-native"
+import { FlatList, Text, StyleSheet, View } from "react-native"
 import CartListItem from "../components/CartListItem"
 import cart from "../data/cart"
 
@@ -7,8 +7,29 @@ const ShoppingCart = () => {
         <FlatList 
             data={cart}
             renderItem={({item}) => <CartListItem cartItem={item}></CartListItem>}
+            ListFooterComponent={() => {
+                <View style={styles.totalContainer}>
+                    <View style={styles.row}>
+                        <Text>Subtotal</Text>
+                        <Text>321,=, 00 US$</Text>
+                    </View>
+                </View>
+            }}
         />
     )
 }
 
 export default ShoppingCart
+
+const styles = StyleSheet.create({
+    totalContainer: {
+        margin: 20,
+        paddingTop: 10,
+        borderColor: 'gainsboro',
+        borderTopWidth: 1,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})

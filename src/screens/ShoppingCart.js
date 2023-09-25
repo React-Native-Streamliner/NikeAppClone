f02e25904eef618dd5683956a6af5ab49c5822b9
tiger <thisisstreamliner@gmail.com>
2023-09-25 18:35:23 +0300

    Finish Styling Shopping Cart List

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,21 +1,34 @@
-import { FlatList, Text, StyleSheet, View } from "react-native"
+import { FlatList, Text, StyleSheet, View, Pressable } from "react-native"
 import CartListItem from "../components/CartListItem"
 import cart from "../data/cart"
 
 const ShoppingCart = () => {
     return (
-        <FlatList 
+        <View>
+            <FlatList 
             data={cart}
             renderItem={({item}) => <CartListItem cartItem={item}></CartListItem>}
             ListFooterComponent={() => {
                 <View style={styles.totalContainer}>
                     <View style={styles.row}>
-                        <Text>Subtotal</Text>
-                        <Text>321,=, 00 US$</Text>
+                        <Text style={styles.text}>Subtotal</Text>
+                        <Text style={styles.text}>321,00 US$</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>Deliver</Text>
+                        <Text style={styles.text}>1,00 US$</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={styles.textBold}>Total</Text>
+                        <Text style={styles.textBold}>322,00 US$</Text>
                     </View>
                 </View>
             }}
-        />
+            />
+            <Pressable onPress={} >
+                <Text>Submit</Text>
+            </Pressable>
+        </View>
     )
 }
 
@@ -30,6 +43,15 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        marginVertical: 2,
+    },
+    text: {
+        fontSize: 16,
+        color: 'gray',
+    },
+    textBold: {
+        fontSize: 16,
+        fontWeight: '500'
     }
 })

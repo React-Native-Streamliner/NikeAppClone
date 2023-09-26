b224a06d22677f4b462b046b69e1c93e9ead7b78
tiger <thisisstreamliner@gmail.com>
2023-09-26 21:32:36 +0300

    Refactor Shopping Cart

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -2,33 +2,35 @@ import { FlatList, Text, StyleSheet, View, Pressable } from "react-native"
 import CartListItem from "../components/CartListItem"
 import cart from "../data/cart"
 
+const ShoppingCartTotals = () => {
+    <View style={styles.totalContainer}>
+        <View style={styles.row}>
+            <Text style={styles.text}>Subtotal</Text>
+            <Text style={styles.text}>321,00 US$</Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}>Deliver</Text>
+            <Text style={styles.text}>1,00 US$</Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.textBold}>Total</Text>
+            <Text style={styles.textBold}>322,00 US$</Text>
+        </View>
+    </View>
+}
+
 const ShoppingCart = () => {
     return (
-        <View>
+        <>
             <FlatList 
             data={cart}
             renderItem={({item}) => <CartListItem cartItem={item}></CartListItem>}
-            ListFooterComponent={() => {
-                <View style={styles.totalContainer}>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Subtotal</Text>
-                        <Text style={styles.text}>321,00 US$</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Deliver</Text>
-                        <Text style={styles.text}>1,00 US$</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.textBold}>Total</Text>
-                        <Text style={styles.textBold}>322,00 US$</Text>
-                    </View>
-                </View>
-            }}
+            ListFooterComponent={ShoppingCartTotals}
             />
-            <Pressable onPress={} >
-                <Text>Submit</Text>
+            <Pressable onPress={} style={styles.button} >
+                <Text style={styles.buttonText}>Checkout</Text>
             </Pressable>
-        </View>
+        </>
     )
 }
 
@@ -53,5 +55,21 @@ const styles = StyleSheet.create({
     textBold: {
         fontSize: 16,
         fontWeight: '500'
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: 'black',
+        bottom: 30,
+        width: '90%',
+        alignSelf: 'center',
+        padding: 20,
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16,
     }
 })
